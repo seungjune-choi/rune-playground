3aa730d4c40214c32f320ea5b7e674930b2ea7a5
june <sjc@marpple.com>
2024-11-04 17:42:26 +0900

    [chore] 패키지 네이밍 mini marpple 로 변경

--- apps/backend/src/app.service.ts
+++ apps/backend/src/app.service.ts
@@ -2,9 +2,6 @@ import { Injectable } from '@libs/decorators';
 
 @Injectable()
 export class AppService {
-  constructor() {
-    console.log('AppService constructor');
-  }
   public healthCheck(): string {
     return 'OK';
   }
